68b91106c564ca2763b2615ea73cae2cb314cc79
kimdodo97 <eh4536@naver.com>
2024-04-11 01:46:21 +0900

    init :setup project

---
+++ src/main/java/com/chamdarae/ChamdaraeApplication.java
@@ -0,0 +1,13 @@
+package com.chamdarae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamdaraeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamdaraeApplication.class, args);
+    }
+
+}
