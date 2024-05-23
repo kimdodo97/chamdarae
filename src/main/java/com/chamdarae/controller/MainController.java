7cfadff69f42f91db11a815e9622d6d78ece1769
kimdodo97 <eh4536@naver.com>
2024-05-24 01:00:13 +0900

    Feat : 화면 제공 컨트롤러 추가

---
+++ src/main/java/com/chamdarae/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.chamdarae.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping(value = "/main")
+    public String thymeleafExample() {
+        System.out.println("main");
+        return "index";
+    }
+}
