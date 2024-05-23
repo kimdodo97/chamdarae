f9b2613143e6d5db6745df9cdc671ffeb3072f8e
kimdodo97 <eh4536@naver.com>
2024-05-23 17:54:25 +0900

    Feat : 사용자 컨트롤러 추가

---
+++ src/main/java/com/chamdarae/controller/MemberController.java
@@ -0,0 +1,53 @@
+package com.chamdarae.controller;
+
+import javax.validation.Valid;
+
+import com.chamdarae.request.MemberCreate;
+import com.chamdarae.request.MemberEdit;
+import com.chamdarae.response.MemberResponse;
+import com.chamdarae.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/member")
+    public void join(@RequestBody @Valid MemberCreate request){
+        Long joinId = memberService.join(request);
+    }
+
+    @GetMapping("/member/{memberId}")
+    public MemberResponse get(@PathVariable Long memberId){
+        return memberService.get(memberId);
+    }
+
+    @PatchMapping("/member/{memberId}")
+    public MemberResponse edit(@PathVariable Long memberId, @RequestBody @Valid MemberEdit request){
+        return memberService.edit(memberId,request);
+    }
+
+    @DeleteMapping("/member/{memberId}")
+    public void quit(@PathVariable Long memberId){
+        memberService.quit(memberId);
+    }
+
+    @GetMapping("/member-email/{email}")
+    public boolean duplicateEmail(@PathVariable String email){
+        return memberService.duplicateEmail(email);
+    }
+
+    @GetMapping("/member-nickname/{nickname}")
+    public boolean duplicateNickname(@PathVariable String nickname){
+        return memberService.duplicateNickname(nickname);
+    }
+
+    @GetMapping("/member-login-id/{loginId}")
+    public boolean duplicateLoginId(@PathVariable String loginId){
+        return memberService.duplicateLoginId(loginId);
+    }
+}
