04acaa32d40e5b88cb913f589e6b442de672a1fd
kimdodo97 <eh4536@naver.com>
2024-05-23 17:45:21 +0900

    Feat : 레시피 엔티티 추가

---
+++ src/main/java/com/chamdarae/domain/Recipe.java
@@ -0,0 +1,28 @@
+package com.chamdarae.domain;
+
+import javax.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Recipe {
+    @Id @GeneratedValue
+    @Column(name="recipe_id")
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    private LocalDate createDate;
+    private LocalDate modifiedDate;
+    private String title;
+    private String content;
+
+    @OneToMany(mappedBy = "recipe")
+    private List<Comment> comments = new ArrayList<>();
+}
